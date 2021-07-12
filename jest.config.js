5481060f72d3c01fd52b4cb21fc7d3001af68289
rom111419 <Eg07yinAl>
2021-07-12 12:04:59 +0300

    JEST new files

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+require('jest-preset-angular/ngcc-jest-processor');
+import('@jest/types').Config;
+
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+	globals: {
+		'ts-jest': {
+			tsconfig: '<rootDir>/tsconfig.spec.json',
+			stringifyContentPathRegex: '\\.html$',
+			allowSyntheticDefaultImports: true,
+		},
+	},
+	displayName: 'darwin',
+	preset: 'jest-preset-angular',
+	setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+	moduleNameMapper: {
+		'^@app/(.*)$': '<rootDir>src/app/$1',
+		'^@env/(.*)$': '<rootDir>src/environments/$1',
+		'^@assets/(.*)$': '<rootDir>src/assets/$1'
+	}
+};
---
+++ src/app/core/global-mocks/style-transform.ts
@@ -0,0 +1,13 @@
+Object.defineProperty(window, 'CSS', { value: null });
+/**
+ * ISSUE: https://github.com/angular/material2/issues/7101
+ * Workaround for JSDOM missing transform property
+ */
+Object.defineProperty(document.body.style, 'transform', {
+	value: () => {
+		return {
+			enumerable: true,
+			configurable: true,
+		};
+	},
+});
